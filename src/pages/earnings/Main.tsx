8fef854a0d0f460fc2514d0177f266d76af67229
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-01-20 19:08:59 +0200

    added more mobile friendly earnings tabs

--- src/pages/earnings/Main.tsx
+++ src/pages/earnings/Main.tsx
@@ -1,4 +1,18 @@
-import {Badge, Flex, Heading, Img, Spacer, Tab, TabList, TabPanel, TabPanels, Tabs, Text, Tag } from '@chakra-ui/react';
+import {
+    Badge,
+    Flex,
+    Heading,
+    Img,
+    Spacer,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+    Tag,
+    useMediaQuery
+} from '@chakra-ui/react';
 import React from 'react';
 import {getTokenUrl} from "../../common/helpers/util";
 import BoardRoomLpTable from "./components/boardroom/BoardRoomLpTable";
@@ -24,6 +38,8 @@ const Main = () => {
     const { data: farmChargeData, isLoading: farmChargeLoading, isError: farmChargeError} = useFarmsChargeEarnings()
     const { data: farmStaticData, isLoading: farmStaticLoading, isError: farmStaticError} = useFarmsStaticEarnings()
 
+    const [isMobile] = useMediaQuery('(max-width: 860px)')
+
     if(!walletAddress){
         return <ConnectDapp/>
     }
@@ -31,25 +47,24 @@ const Main = () => {
     return (
         <Flex flexDir="column" py={5} h="100%">
             <Tabs isFitted w="100%">
-                <TabList w="100%" overflow={"auto"}>
-                    <Tab  _focus={{outline: "none"}}>
+                <TabList w="100%" flexWrap="wrap" flexDir={isMobile ? "column": "row"} >
+                    <Tab  _focus={{outline: "none"}} px={3} >
                         <Img src={getTokenUrl("static-busd")} w="50px" h="30px"/>
                         <Text px={2}>Static-BUSD Boardroom</Text>
                         <Tag borderRadius='full' textColor="white" colorScheme="blue">{brLPData && brLPData.length > 0 ? brLPData.length : "Empty" }</Tag>
-
                     </Tab>
-                    <Tab _focus={{outline: "none"}}>
+                    <Tab _focus={{outline: "none"}} px={3}>
                         <Img src={getTokenUrl("charge")} w="30px" h="30px"/>
                         <Text px={2}>Charge Boardroom</Text>
                         <Tag borderRadius='full' textColor="white" colorScheme="blue">{brChargeData && brChargeData.length > 0 ? brChargeData.length : "Empty" }</Tag>
 
                     </Tab>
-                    <Tab  _focus={{outline: "none"}}>
+                    <Tab  _focus={{outline: "none"}} px={3}>
                         <Img src={getTokenUrl("charge-busd")} w="50px" h="30px"/>
                         <Text px={2}>Charge-BUSD Farm</Text>
                         <Tag borderRadius='full' textColor="white" colorScheme="blue">{farmChargeData && farmChargeData.length > 0 ? farmChargeData.length : "Empty" }</Tag>
                     </Tab>
-                    <Tab  _focus={{outline: "none"}}>
+                    <Tab  _focus={{outline: "none"}} px={3}>
                         <Img src={getTokenUrl("static-busd")} w="50px" h="30px"/>
                         <Text px={2}>Static-BUSD Farm</Text>
                         <Tag borderRadius='full' textColor="white" colorScheme="blue">{farmStaticData && farmStaticData.length > 0 ? farmStaticData.length : "Empty" }</Tag>
