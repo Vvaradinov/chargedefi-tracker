0153a1a0b4ae5208e76f957f8f0b2fe1746fd834
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-04-20 22:49:58 +0300

    forgot to update api endpoin

--- src/pages/protocol/api.ts
+++ src/pages/protocol/api.ts
@@ -1,10 +1,11 @@
 import axios, {AxiosRequestConfig} from "axios";
+import {defaultChain} from "../../config";
 
 export const getFarmsEarnings = async(): Promise<any> => {
     const config: AxiosRequestConfig = {
         baseURL: process.env.REACT_APP_DEV_API_URL,
         method: "get",
-        url: "protocol-history",
+        url: defaultChain.shortName === "BSC" ? "protocol-history" : "protocol-history-ftm",
     }
     const response = await axios(config);
     return response.data;
